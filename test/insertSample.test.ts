62eb9a97e6535555d4ef90844a98981d342c5417
donghquinn <ehdgus1524@gmail.com>
2024-03-30 17:45:51 +0900

    test code

--- test/insertSample.test.ts
+++ test/insertSample.test.ts
@@ -9,21 +9,34 @@ describe('Check DB Connection and Query Data', () => {
         expect( result ).toBeUndefined();
     } );
 
-    test( "INSERT", async () =>
+    test( "CREATE USER", async () =>
     {
-        const result = await mysql.query( `
-        CREATE TABLE table_user (
-            user_id         VARCHAR(50)     NOT NULL,
-            user_name       VARCHAR(50)     NULL,
-            user_email      VARCHAR(50)     NOT NULL,
-            user_password   VARCHAR(50)     NOT NULL,
-            user_status     VARCHAR(10)     NOT NULL,
+        const userResult = await mysql.query( `
+            CREATE TABLE table_user (
+                user_id         VARCHAR(50)     NOT NULL,
+                user_name       VARCHAR(50)     NULL,
+                user_email      VARCHAR(50)     NOT NULL,
+                user_password   VARCHAR(50)     NOT NULL,
+                user_status     VARCHAR(10)     NOT NULL,
 
-            PRIMARY KEY user_id
-        );
-`);
+                PRIMARY KEY user_id
+            );
+        `);
         
-        expect( result ).toBeDefined();
+        expect( userResult ).toBeDefined();
+    } );
+
+    test( "CREATE SESSION", async () => {
+        const sessionResult = await mysql.query( `
+            CREATE TABLE table_user_session (
+                session_id      VARCHAR(50)     NOT NULL,
+                user_id         VARCHAR(50)     NOT NULL        REFERENCES  table_user(user_id),
+
+                PRIMARY KEY session_id
+            );
+        `);
+        
+        expect( sessionResult ).toBeDefined();
     } );
 
   test('Query', async() => {
