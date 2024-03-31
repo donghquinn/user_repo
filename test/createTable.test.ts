fe7ca11c2fc8685298fc30d0cb40437f49ccbb02
donghquinn <ehdgus1524@gmail.com>
2024-03-31 10:53:40 +0900

    changing table

--- test/createTable.test.ts
+++ test/createTable.test.ts
@@ -13,15 +13,13 @@ describe('Check DB Connection and Query Data', () => {
     test( "CREATE USER TABLE", async () => {
         const userResult = await mysql.query( `
             CREATE TABLE user_table (
-                user_id         VARCHAR(50)     NOT NULL,
+                user_id         VARCHAR(50)     NOT NULL     PRIMARY KEY,
                 user_name       VARCHAR(50)     NULL,
                 user_email      VARCHAR(50)     NOT NULL,
                 user_password   VARCHAR(50)     NOT NULL,
                 user_status     VARCHAR(10)     NOT NULL,
-                user_type       TINYINT(1)      NOT NULL DEFAULT 1,
-                reg_date        DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP
-
-                PRIMARY KEY user_id
+                user_type       TINYINT(1)      NOT NULL      DEFAULT 1,
+                reg_date        DATETIME        NOT NULL      DEFAULT CURRENT_TIMESTAMP
             )
         `);
         
@@ -31,11 +29,9 @@ describe('Check DB Connection and Query Data', () => {
     test( "CREATE SESSION TABLE", async () => {
         const sessionResult = await mysql.query( `
             CREATE TABLE user_table_session (
-                session_id      VARCHAR(50)     NOT NULL,
+                session_id      VARCHAR(50)     NOT NULL        PRIMARY KEY,
                 user_id         VARCHAR(50)     NOT NULL        REFERENCES  table_user(user_id),
-                reg_date        DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP
-
-                PRIMARY KEY session_id
+                reg_date        DATETIME        NOT NULL        DEFAULT CURRENT_TIMESTAMP
             )
         `);
         
