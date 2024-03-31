2c31dea120dccd73340674840ed11af68c28e332
donghquinn <ehdgus1524@gmail.com>
2024-04-01 00:51:20 +0900

    port

--- src/configs/ServerConfig.ts
+++ src/configs/ServerConfig.ts
@@ -3,7 +3,7 @@ export const dbConnectConfig = {
   user: process.env.MARIADB_USER,
   password: process.env.MARIADB_PASSWORD,
   database: process.env.MARIADB_DATABASE,
-  port: Number( process.env.MARIADB_PORT ),
+  port: Number(process.env.MARIADB_PORT),
   waitForConnections: true,
   connectionLimit: 30,
   queueLimit: 0,
