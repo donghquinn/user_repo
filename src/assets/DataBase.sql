dd5fd7c5939b7f19d780a559ee71620ec5b42574
donghquinn <ehdgus1524@gmail.com>
2024-03-31 19:38:48 +0900

    wip: query test finished

--- src/assets/DataBase.sql
+++ src/assets/DataBase.sql
@@ -1,14 +1,15 @@
-CREATE TABLE table_user (
+CREATE TABLE uesr_table (
     user_id         VARCHAR(50)     NOT NULL,
     user_name       VARCHAR(50)     NULL,
     user_email      VARCHAR(50)     NOT NULL,
     user_password   VARCHAR(50)     NOT NULL,
-    user_status     VARCHAR(10)     NOT NULL,
+    user_status     VARCHAR(10)     NOT NULL    DEFAULT "10",
+    user_type       TINYINT         NOT NULL    DEFAULT 1,
 
     PRIMARY KEY user_id
 );
 
-CREATE TABLE table_user_session (
+CREATE TABLE user_table__session (
     session_id      VARCHAR(50)     NOT NULL,
     user_id         VARCHAR(50)     NOT NULL        REFERENCES  table_user(user_id),
 
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,31 @@
 /* eslint-disable max-len */
 import { MySqlInstance } from "@libraries/Database";
 import express from 'express';
-import { createHash } from "node:crypto";
-import { escape } from 'mysql2';
-import { randomUUID } from "crypto";
 
 const mysql = MySqlInstance.getInstance();
 
 await mysql.start();
 
 
-const encodedEmail = createHash( "sha256" ).update( "test@example.com" ).digest( "base64" );
-const encodedPassword = createHash( "sha256" ).update( "1234" ).digest( "base64" );
+// const encodedEmail = createHash( "sha256" ).update( "test@example.com" ).digest( "base64" );
+// const encodedPassword = createHash( "sha256" ).update( "1234" ).digest( "base64" );
+// const encodedName = createHash( "sha256" ).update( "test" ).digest( "base64" );
 
-const userId = randomUUID();
+// const userId = randomUUID();
 
-await mysql.query( `
-INSERT INTO user_table (user_id, user_email, user_name, user_password, user_status, user_type)
-VALUES
-("${ userId }", ${ escape( encodedEmail ) }, "test", ${ escape( encodedPassword ) }, "10", 1 )` );
+// await mysql.query( `
+//     INSERT INTO user_table (user_id, user_email, user_name, user_password)
+//     VALUES
+//     ("${ userId }", ${ escape( encodedEmail ) }, ${escape(encodedName) }, ${ escape( encodedPassword ) })`
+// );
 
 
-const queryString =
-    `SELECT * FROM user_table WHERE user_email = ${ escape(encodedEmail) } AND user_password = ${ escape(encodedPassword) }`;
+// const queryString =
+//     `SELECT * FROM user_table WHERE user_email = ${ escape(encodedEmail) } AND user_password = ${ escape(encodedPassword) }`;
 
-const [result] = await mysql.query( queryString );
+// const [result] = await mysql.query( queryString );
 
-console.log( "Result: %o", { result } );
+// console.log( "Result: %o", { result } );
 
 const app = express();
 
