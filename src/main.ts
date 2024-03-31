93c31c24a9c72b411f450fc201268e95dd2cfd05
donghquinn <ehdgus1524@gmail.com>
2024-03-31 19:44:17 +0900

    wip: clearifying apis

--- src/main.ts
+++ src/main.ts
@@ -6,32 +6,14 @@ const mysql = MySqlInstance.getInstance();
 
 await mysql.start();
 
-
-// const encodedEmail = createHash( "sha256" ).update( "test@example.com" ).digest( "base64" );
-// const encodedPassword = createHash( "sha256" ).update( "1234" ).digest( "base64" );
-// const encodedName = createHash( "sha256" ).update( "test" ).digest( "base64" );
-
-// const userId = randomUUID();
-
-// await mysql.query( `
-//     INSERT INTO user_table (user_id, user_email, user_name, user_password)
-//     VALUES
-//     ("${ userId }", ${ escape( encodedEmail ) }, ${escape(encodedName) }, ${ escape( encodedPassword ) })`
-// );
-
-
-// const queryString =
-//     `SELECT * FROM user_table WHERE user_email = ${ escape(encodedEmail) } AND user_password = ${ escape(encodedPassword) }`;
-
-// const [result] = await mysql.query( queryString );
-
-// console.log( "Result: %o", { result } );
-
 const app = express();
 
 app.get( "/", ( req, res ) => {
     res.json( { message: "success" } );
 } );
+
+app.post( "/api/signup" );
+app.post( "/api/login" );
 
 app.listen( 6308, () => {
     console.log( "Listening On 6308" );
