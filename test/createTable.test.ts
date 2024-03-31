08b5fd1c4f3a6c5d1485fa9fb6172849ab2307b9
donghquinn <ehdgus1524@gmail.com>
2024-03-31 10:48:49 +0900

    wip: test creating

--- test/createTable.test.ts
+++ test/createTable.test.ts
@@ -19,6 +19,7 @@ describe('Check DB Connection and Query Data', () => {
                 user_password   VARCHAR(50)     NOT NULL,
                 user_status     VARCHAR(10)     NOT NULL,
                 user_type       TINYINT(1)      NOT NULL DEFAULT 1,
+                reg_date        DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP
 
                 PRIMARY KEY user_id
             )
@@ -32,6 +33,7 @@ describe('Check DB Connection and Query Data', () => {
             CREATE TABLE user_table_session (
                 session_id      VARCHAR(50)     NOT NULL,
                 user_id         VARCHAR(50)     NOT NULL        REFERENCES  table_user(user_id),
+                reg_date        DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP
 
                 PRIMARY KEY session_id
             )
